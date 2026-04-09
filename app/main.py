cb2fec1d7fb0614502ad1cab490f65ce1febbd22
OmPatel01 <ompatel2587@gmail.com>
2026-04-09 21:37:43 +0530

    fixed main.py CORS deployment issue

--- app/main.py
+++ app/main.py
@@ -63,7 +63,12 @@ app = FastAPI(
 # ── CORS — allows the frontend HTML to call this API ─────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # restrict to your domain in production
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://credit-risk-modelling-two.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
